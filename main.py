e18882d63a5a998530f7a577e2c5263a25e1c4b0
Kate <katelocat3@gmail.com>
2025-12-15 15:19:16 +0100

    Add class NotificationAsyncIO with notify method.

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import asyncio
 import sys
 
-from notification_manager import create_notification, wait_for_notification
+from notification_crud_operations import NotificationManager
 
-async def async_input(string: str) -> str:
-    await asyncio.to_thread(sys.stdout.write, f'{string} ')
-    return (await asyncio.to_thread(sys.stdin.readline)).rstrip('\n')
 
-async def wait_for_input():
-    message = await async_input("Enter your message:")
-    timedelta = int(await async_input("Enter time in seconds:"))
-    create_notification(timedelta, message)
+class NotificationAsyncIO:
+    def __init__(self):
+        self.notification_manager = NotificationManager()
+
+    @staticmethod
+    async def async_input(string: str) -> str:
+        await asyncio.to_thread(sys.stdout.write, f'{string} ')
+        return (await asyncio.to_thread(sys.stdin.readline)).rstrip('\n')
+
+    async def wait_for_input(self):
+        message = await self.async_input("Enter your message:")
+        timedelta = int(await self.async_input("Enter time in seconds:"))
+
+        self.notification_manager.create_notification(timedelta, message)
+
+    async def notify(self):
+        notification = self.notification_manager.get_next_notification()
+        if notification:
+            await notification.wait_for_due()
+            print(notification.message)
+
 
 async def main():
-    while True:
-        async with asyncio.TaskGroup() as tg:
-            tg.create_task(wait_for_input())
-            tg.create_task(wait_for_notification())
+    notification_io = NotificationAsyncIO()
+    try:
+        while True:
+            # I suppose that one of the problems is hidden here:
+            # loop should not enter context manager each iteration,
+            # that's my best guess for why timer is blocking everything.
+            async with asyncio.TaskGroup() as tg:
+                tg.create_task(notification_io.notify())
+                tg.create_task(notification_io.wait_for_input())
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == "__main__":
