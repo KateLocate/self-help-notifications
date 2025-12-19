f6d1b83660057daf7ff84fd5980861712d43feff
Kate <katelocat3@gmail.com>
2025-12-19 16:37:15 +0100

    Fix asyncio issue.
    There was a bug that interrupted infinite loop and prevented tasks from concurrent execution.
    By rearranging code and using one task instead of task group instance the issue was solved.

--- src/notification_service/main.py
+++ src/notification_service/main.py
@@ -8,34 +8,29 @@ class NotificationAsyncIO:
     def __init__(self):
         self.notification_manager = NotificationManager()
 
-    @staticmethod
-    async def async_input(string: str) -> str:
-        await asyncio.to_thread(sys.stdout.write, f'{string} ')
-        return (await asyncio.to_thread(sys.stdin.readline)).rstrip('\n')
-
     async def wait_for_input(self):
-        message = await self.async_input("Enter your message:")
-        timedelta = int(await self.async_input("Enter time in seconds:"))
+        user_input = await asyncio.to_thread(input, "Enter your message:")
 
-        self.notification_manager.create_notification(timedelta, message)
+        if not user_input.strip():
+            return
+
+        timedelta = int(await asyncio.to_thread(input,"Enter time in seconds:"))
+
+        self.notification_manager.create_notification(timedelta, user_input)
+
+        await self.notify()
 
     async def notify(self):
         notification = self.notification_manager.get_next_notification()
         if notification:
-            await notification.wait_for_due()
-            print(notification.message)
+            asyncio.create_task(notification.wait_then_output())
 
 
 async def main():
     notification_io = NotificationAsyncIO()
     try:
         while True:
-            # I suppose that one of the problems is hidden here:
-            # loop should not enter context manager each iteration,
-            # that's my best guess for why timer is blocking everything.
-            async with asyncio.TaskGroup() as tg:
-                tg.create_task(notification_io.notify())
-                tg.create_task(notification_io.wait_for_input())
+            await notification_io.wait_for_input()
     except KeyboardInterrupt:
         pass
 
