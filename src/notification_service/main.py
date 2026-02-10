b20d85fb4975e4fff3fcf743684bd998185adf53
Kate <katelocat3@gmail.com>
2026-02-10 19:24:38 +0100

    Add FastApi app with two handlers, remove main function.

--- src/notification_service/main.py
+++ src/notification_service/main.py
@@ -4,26 +4,6 @@ from notification_crud_operations import NotificationManager
 
 from fastapi import FastAPI, HTTPException
 
-app = FastAPI()
-notification_manager = NotificationManager()
-
-@app.get("/received_notifications")
-def get_received_notifications():
-    notifications = notification_manager.get_ready_notifications()
-    return {"notifications": notifications}
-
-
-@app.post("/notification")
-def add_notification(body: dict):
-    # {"text": "hi", "timedelta": 5}
-    notification_text = body.get("text")
-    notification_timedelta = body.get("timedelta")
-    if not notification_text or not notification_timedelta:
-        raise HTTPException(status_code=400, detail="'text' and 'timedelta' fields both are required")
-
-    notification_manager.create_notification(delta=notification_timedelta, message=notification_text)
-    return {"message": "Item added successfully", "item": notification_text}
-
 
 class NotificationAsyncIO:
     def __init__(self):
@@ -39,21 +19,31 @@ class NotificationAsyncIO:
 
         self.notification_manager.create_notification(timedelta, user_input)
 
-        await self.notify()
+        await self.get_next_notification_in_time()
 
-    async def notify(self):
+    async def get_next_notification_in_time(self) -> str:
         if notification := self.notification_manager.get_next_notification():
-            asyncio.create_task(notification.wait_then_output())
+            await asyncio.create_task(notification.wait_then_output())
+            return notification.message
 
 
-async def main():
-    notification_io = NotificationAsyncIO()
-    try:
-        while True:
-            await notification_io.wait_for_input()
-    except KeyboardInterrupt:
-        pass
+app = FastAPI()
+notificationsIO = NotificationAsyncIO()
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+@app.get("/received_notifications")
+def get_received_notifications():
+    notifications = notificationsIO.notification_manager.get_ready_notifications()
+    return {"notifications": notifications}
+
+
+@app.post("/notification")
+def add_notification(body: dict):
+    # {"text": "hi", "timedelta": 5}
+    notification_text = body.get("text")
+    notification_timedelta = body.get("timedelta")
+    if not notification_text or not notification_timedelta:
+        raise HTTPException(status_code=400, detail="'text' and 'timedelta' fields both are required")
+
+    notificationsIO.notification_manager.create_notification(delta=notification_timedelta, message=notification_text)
+    return {"message": "Item added successfully", "item": notification_text}
