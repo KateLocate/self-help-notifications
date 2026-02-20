c38d7040ec36fd5adadbb60a0970f0af6728d1a0
Kate <katelocat3@gmail.com>
2026-02-20 17:46:04 +0100

    Fix: move notification_ops into container.

--- src/notification_service/main.py
+++ src/notification_service/main.py
@@ -3,7 +3,6 @@ import asyncio
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, WebSocket, HTTPException
 
-from constants import MESSAGE
 from notification_repository import NotificationOperations, MongoManager, NotificationRepository
 
 
@@ -12,7 +11,7 @@ class Container:
     def __init__(self, mongo_uri: str, db_name: str):
         self.mongo = MongoManager(mongo_uri, db_name)
         self.notification_repo = NotificationRepository(self.mongo)
-
+        self.notification_ops = NotificationOperations(self.notification_repo.collection)
 
 container = Container(
     mongo_uri='mongodb://localhost:27017',
@@ -35,19 +34,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-notification_ops = NotificationOperations(container.notification_repo.collection)
 
 
 @app.get('/received_notifications')
-def get_received_notifications():
-    notifications = notification_ops.get_ready_notifications()
+async def get_received_notifications():
+    notifications = await container.notification_ops.get_ready_notifications()
     return {'notifications': notifications}
 
 @app.websocket('/ws')
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        data = await notification_ops.get_next_notification_in_time()
+        data = await container.notification_ops.get_next_notification_in_time()
         if data:
             await websocket.send_text(f'Notification text: {data}')
         else:
@@ -57,10 +55,10 @@ async def websocket_endpoint(websocket: WebSocket):
 @app.post('/notification')
 async def add_notification(body: dict):
     # e.g. {"message": "hi", "timedelta": 5}
-    notification_text = body.get(MESSAGE)
+    notification_text = body.get('message')
     notification_timedelta = body.get('timedelta')
     if not notification_text or not notification_timedelta:
         raise HTTPException(status_code=400, detail='Both "text" and "timedelta" fields are required')
 
-    await notification_ops.create_notification(due_time=notification_timedelta, message=notification_text)
-    return {MESSAGE: 'Item added successfully', 'item': notification_text}
+    await container.notification_ops.create_notification(due_time=notification_timedelta, message=notification_text)
+    return {'message': 'Item added successfully', 'item': notification_text}
