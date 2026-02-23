b54901db6ffdb6ba294f765c2bbbcfd83db11654
Kate <katelocat3@gmail.com>
2026-02-23 18:43:45 +0100

    Refactor: remove code that doesn't belong to main.py.

--- src/notification_service/main.py
+++ src/notification_service/main.py
@@ -1,22 +1,8 @@
-import asyncio
-
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, WebSocket, HTTPException
+from fastapi import FastAPI
 
-from notification_service.notification_repository import NotificationOperations, MongoManager, NotificationRepository
-
-
-class Container:
-
-    def __init__(self, mongo_uri: str, db_name: str):
-        self.mongo = MongoManager(mongo_uri, db_name)
-        self.notification_repo = NotificationRepository(self.mongo)
-        self.notification_ops = NotificationOperations(self.notification_repo.collection)
-
-container = Container(
-    mongo_uri='mongodb://localhost:27017',
-    db_name='notifications_db'
-)
+from notification_service.container import container
+from notification_service.routers import notifications
 
 
 @asynccontextmanager
@@ -33,32 +19,10 @@ async def lifespan(app: FastAPI):
     await mongo.close()
 
 
+def include_all_routes(app):
+    app.include_router(notifications.router)
+
+
+# Application startup
 app = FastAPI(lifespan=lifespan)
-
-
-@app.get('/received_notifications')
-async def get_received_notifications():
-    notifications = await container.notification_ops.get_ready_notifications()
-    return {'notifications': notifications}
-
-@app.websocket('/ws')
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await container.notification_ops.get_next_notification_in_time()
-        if data:
-            await websocket.send_text(f'Notification text: {data}')
-        else:
-            await asyncio.sleep(1)
-
-
-@app.post('/notification')
-async def add_notification(body: dict):
-    # e.g. {"message": "hi", "timedelta": 5}
-    notification_text = body.get('message')
-    notification_timedelta = body.get('timedelta')
-    if not notification_text or not notification_timedelta:
-        raise HTTPException(status_code=400, detail='Both "text" and "timedelta" fields are required')
-
-    await container.notification_ops.create_notification(due_time=notification_timedelta, message=notification_text)
-    return {'message': 'Item added successfully', 'item': notification_text}
+include_all_routes(app)
